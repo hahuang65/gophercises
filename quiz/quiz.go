e74bc0612bc5a4000fee66d66d2b28a204479e13
Howard Huang <hao@hwrd.me>
2022-04-22 21:40:44 -0700

    Implement bonus exercises for quiz
    * Add string trimming and cleanup to help ensure that correct answers with extra whitespace, capitalization, etc are not considered incorrect. Hint: Check out the strings package.
    * Add an option (a new flag) to shuffle the quiz order each time it is run.

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -3,6 +3,9 @@ package quiz
 import (
 	"flag"
 	"fmt"
+	"math/rand"
+	"strings"
+	"time"
 
 	"git.sr.ht/~hwrd/gophercises/util"
 )
@@ -14,15 +17,19 @@ func (q *Quiz) CommandName() string {
 }
 
 func (q *Quiz) Run(args []string) {
-	var csvPath string
+	var (
+		csvPath string
+		shuffle bool
+	)
 
 	cmd := flag.NewFlagSet(q.CommandName(), flag.ExitOnError)
 	cmd.StringVar(&csvPath, "csv", fmt.Sprintf("%s/problems.csv", q.CommandName()), "filepath to the CSV containing quiz questions")
+	cmd.BoolVar(&shuffle, "shuffle", false, "Shuffle the order of the problems")
 	cmd.Parse(args)
 
 	lines := util.ReadCSV(csvPath)
 	problems := parseProblems(lines)
-	score := poseProblems(problems)
+	score := poseProblems(problems, shuffle)
 
 	fmt.Printf("Total questions: %d, Score: %d\n", len(problems), score)
 }
@@ -38,21 +45,28 @@ func parseProblems(lines [][]string) []problem {
 	for i, line := range lines {
 		problems[i] = problem{
 			question: line[0],
-			answer:   line[1],
+			answer:   strings.TrimSpace(line[1]),
 		}
 	}
 
 	return problems
 }
 
-func poseProblems(problems []problem) int {
+func poseProblems(problems []problem, shuffle bool) int {
 	score := 0
+
+	if shuffle {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(problems), func(i, j int) {
+			problems[i], problems[j] = problems[j], problems[i]
+		})
+	}
 
 	for _, problem := range problems {
 		fmt.Printf("%s = ", problem.question)
 		var answer string
 		fmt.Scanf("%s\n", &answer)
-		if answer == problem.answer {
+		if strings.TrimSpace(answer) == problem.answer {
 			score++
 		}
 	}
